a769c27be07db7a4916ba151f56a047f8b085272
SameetPathan <sameetpathanrs@gmail.com>
2024-03-24 02:24:49 +0530

    v5

--- src/components/Admingov.js
+++ src/components/Admingov.js
@@ -8,11 +8,21 @@ function Admingov(props) {
   const [fixedbottom, setfixedbottom] = useState(true);
   return (
     <div>
-       <div class="alert alert-dark" role="alert">
- <b>Keep a track on products ,</b> <hr></hr> 
- 1. What products are been sold in market ? <hr></hr>
- 2. For what price the products are been sold ?<hr></hr>
- 3. Which products orders are been Accepted/Rejected ?
+    <div class="alert alert-dark" role="alert">
+    <p><b>Keep a track on products</b></p>
+    <hr />
+    <p>Tracking products in the market is crucial for any business. It allows you to stay updated on what is currently available and in demand, enabling informed decision-making and strategizing.</p>
+    <hr />
+    <ol>
+        <li><b>What products are being sold in the market?</b></li>
+        <p>Knowing which products are being sold in the market helps in understanding consumer preferences and trends. It aids in adjusting inventory levels and introducing new products to meet market demands.</p>
+        <hr />
+        <li><b>For what price are the products being sold?</b></li>
+        <p>Understanding the pricing of products provides insights into market competitiveness and helps in setting competitive prices for your own products. Monitoring price fluctuations also assists in identifying potential opportunities or threats in the market.</p>
+        <hr />
+        <li><b>Which products' orders are being accepted/rejected?</b></li>
+        <p>Tracking the acceptance or rejection of orders for different products is essential for managing inventory and customer satisfaction. It helps in identifying popular products with high demand as well as those that may require adjustments in pricing or marketing strategies.</p>
+    </ol>
 </div>
 <hr />
 <hr />
--- src/components/UserProductView.js
+++ src/components/UserProductView.js
@@ -5,6 +5,252 @@ import Loader from './Loader';
 
 
 
+const DealerContractAddress = "0x09C1be1Bc686241636AdAD5bBAc6d2Bf44b01C9d";
+const abiDealerContract = [
+  {
+	"inputs": [
+	  {
+		"internalType": "address",
+		"name": "UserAddress",
+		"type": "address"
+	  },
+	  {
+		"internalType": "string",
+		"name": "name",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "dealeraddress",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "phoneNumber",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "ProductName",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "imagepath",
+		"type": "string"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "Quantity",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "negotiatedPrice",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "acceptedStatus",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "productId",
+		"type": "uint256"
+	  }
+	],
+	"name": "addProduct",
+	"outputs": [],
+	"stateMutability": "nonpayable",
+	"type": "function"
+  },
+  {
+	"inputs": [
+	  {
+		"internalType": "uint256",
+		"name": "buyid",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "status",
+		"type": "uint256"
+	  }
+	],
+	"name": "updateDriver",
+	"outputs": [],
+	"stateMutability": "nonpayable",
+	"type": "function"
+  },
+  {
+	"inputs": [],
+	"name": "getAllProducts",
+	"outputs": [
+	  {
+		"components": [
+		  {
+			"internalType": "uint256",
+			"name": "buyid",
+			"type": "uint256"
+		  },
+		  {
+			"internalType": "address",
+			"name": "UserAddress",
+			"type": "address"
+		  },
+		  {
+			"internalType": "string",
+			"name": "name",
+			"type": "string"
+		  },
+		  {
+			"internalType": "string",
+			"name": "dealeraddress",
+			"type": "string"
+		  },
+		  {
+			"internalType": "string",
+			"name": "phoneNumber",
+			"type": "string"
+		  },
+		  {
+			"internalType": "string",
+			"name": "ProductName",
+			"type": "string"
+		  },
+		  {
+			"internalType": "string",
+			"name": "imagepath",
+			"type": "string"
+		  },
+		  {
+			"internalType": "uint256",
+			"name": "Quantity",
+			"type": "uint256"
+		  },
+		  {
+			"internalType": "uint256",
+			"name": "negotiatedPrice",
+			"type": "uint256"
+		  },
+		  {
+			"internalType": "uint256",
+			"name": "acceptedStatus",
+			"type": "uint256"
+		  },
+		  {
+			"internalType": "uint256",
+			"name": "productId",
+			"type": "uint256"
+		  }
+		],
+		"internalType": "struct DealerBuyContract.DealerData[]",
+		"name": "",
+		"type": "tuple[]"
+	  }
+	],
+	"stateMutability": "view",
+	"type": "function"
+  },
+  {
+	"inputs": [],
+	"name": "getNumberOfRecords",
+	"outputs": [
+	  {
+		"internalType": "uint256",
+		"name": "",
+		"type": "uint256"
+	  }
+	],
+	"stateMutability": "view",
+	"type": "function"
+  },
+  {
+	"inputs": [],
+	"name": "nextProductId",
+	"outputs": [
+	  {
+		"internalType": "uint256",
+		"name": "",
+		"type": "uint256"
+	  }
+	],
+	"stateMutability": "view",
+	"type": "function"
+  },
+  {
+	"inputs": [
+	  {
+		"internalType": "uint256",
+		"name": "",
+		"type": "uint256"
+	  }
+	],
+	"name": "products",
+	"outputs": [
+	  {
+		"internalType": "uint256",
+		"name": "buyid",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "address",
+		"name": "UserAddress",
+		"type": "address"
+	  },
+	  {
+		"internalType": "string",
+		"name": "name",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "dealeraddress",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "phoneNumber",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "ProductName",
+		"type": "string"
+	  },
+	  {
+		"internalType": "string",
+		"name": "imagepath",
+		"type": "string"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "Quantity",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "negotiatedPrice",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "acceptedStatus",
+		"type": "uint256"
+	  },
+	  {
+		"internalType": "uint256",
+		"name": "productId",
+		"type": "uint256"
+	  }
+	],
+	"stateMutability": "view",
+	"type": "function"
+  }
+];
+
+
 
 const ProductContractAddress="0x1A53896aE0281eda0b2793fA8f321F944584D2d5";
 const abiProductContract=[
@@ -320,14 +566,19 @@ const abiProductContract=[
 var arraylist2 = [];
 var whole2 = [];
 
+var arraylist3 = [];
+var whole3 = [];
+
+
 function UserProductView(props) {
 
 	const [account, setAccount] = useState(null);
 
 	const [w2, setw2] = useState([]);
+	const [w3, setw3] = useState([]);
 	const [filteredProducts, setFilteredProducts] = useState([]);
 	const [averageProducts, setaverageProducts] = useState([]);
-
+	const [acceptedProductNames, setacceptedProductNames] = useState([]);
 	const setacc=async()=>{
 
 		const { ethereum } = window;
@@ -338,7 +589,6 @@ function UserProductView(props) {
 	  }
 
       const averagePricesByName = (productsList) => {
-		debugger
         const productsMap = new Map();
         productsList.forEach((product) => {
           const name = product[7]; // Assuming product name is at index 8
@@ -364,6 +614,43 @@ function UserProductView(props) {
 		setFilteredProducts(averagedProducts)
         return averagedProducts;
       };
+
+
+
+	  async function getproducts() {
+		try {
+
+		
+		  const { ethereum } = window;
+	
+		  if (ethereum) {
+			const provider = new ethers.providers.Web3Provider(ethereum);
+			const signer = provider.getSigner();
+	
+			console.log(2);
+			const ProductContract = new ethers.Contract(
+				DealerContractAddress,
+                abiDealerContract,
+			  signer
+			);
+			arraylist3 = await ProductContract.getAllProducts();
+			console.log(arraylist3);
+			for (var i = 0; i < arraylist3.length; i++) {
+			  whole3[i] = arraylist3[i];
+			}
+			setw3(whole3);
+			console.log("### Data accepoted:",whole3)
+			const acceptedProductNames = whole3
+  .filter(record => String(record[9]) ==="1") // Filter accepted products
+  .map(record => String(record[5])); // Map to get only the product names
+
+console.log("##acceptedProductNames:",acceptedProductNames);
+setacceptedProductNames(acceptedProductNames);
+		  }
+		} catch (error) {
+		  console.error(error);
+		}
+	  }
 
       
 
@@ -398,6 +685,7 @@ function UserProductView(props) {
 	  useEffect(() => {
         props.setfixedbottom(false);
 		getAllproducts();
+		getproducts();
 		setacc();
 	},[]);
 
@@ -423,16 +711,24 @@ function UserProductView(props) {
 		setFilteredProducts(updatedFilteredProducts);
 	  };
 
-	  
-  
+
   return (
 	<div>
 	<Loader></Loader>
 	<div class="alert alert-dark" role="alert">
- <b>Keep a track on products ,</b> <hr></hr> 
- 1. What products are been sold in market ? <hr></hr>
- 2. Checkout at what Avegare price the product must be available in market.<hr></hr>
+    <p><b>Keep track of products</b></p>
+	<hr />
+    <p>Monitoring the products available in the market is essential for businesses to stay competitive and informed about consumer preferences and market trends.</p>
+    <hr />
+    <ol>
+        <li><b>What products are being sold in the market?</b></li>
+        <p>Identifying the products being sold in the market provides valuable insights into consumer demand and market trends. This information can help businesses tailor their offerings to meet customer needs more effectively.</p>
+		<hr />
+        <li><b>Checkout at what average price the product must be available in the market.</b></li>
+        <p>Determining the average price of a product in the market is crucial for pricing strategies and competitive analysis. It allows businesses to assess their pricing strategy's effectiveness and make adjustments to remain competitive.</p>
+    </ol>
 </div>
+
 
 	<div className="container-fluid shadow-lg p-3 mb-5 bg-white rounded mt-3">
 
@@ -446,38 +742,45 @@ function UserProductView(props) {
 	
 
 
-  {filteredProducts.map((record, index) => (
-    <div className="card mb-5">
-      <div className="card-header bg-dark text-white">
-        Product Information
-      </div>
-      <div className="card-body d-flex">
-        <div className="col-sm-8">
-		<dl className="row">
-			<dt className="col-sm-3">Product Name</dt>
-			<dd className="col-sm-9" id="pname">{record.name}</dd>
+  {filteredProducts.map((record, index) => {
+	// Check if the product name is present in the data array
+	if (acceptedProductNames.includes(record.name)) {
+	  return (
+		<div className="card mb-5" key={index}>
+		  <div className="card-header bg-dark text-white">
+			Product Information
+		  </div>
+		  <div className="card-body d-flex">
+			<div className="col-sm-8">
+			  <dl className="row">
+				<dt className="col-sm-3">Product Name</dt>
+				<dd className="col-sm-9" id="pname">{record.name}</dd>
+	
+				<dt className="col-sm-3">Type</dt>
+				<dd className="col-sm-9" id="type">{record.Type}</dd>
   
-			<dt className="col-sm-3">Type</dt>
-			<dd className="col-sm-9" id="type">{record.Type}</dd>
-
-            <dt className="col-sm-3">Average Price</dt>
-            <dd className="col-sm-9" id="averageprice">{record.averagePrice}</dd>
+				<dt className="col-sm-3">Average Price</dt>
+				<dd className="col-sm-9" id="averageprice">{record.averagePrice}</dd>
+			  </dl>
+			</div>
+			<div className="col-sm-4">
+			  <img
+				src={record.imagepath}
+				alt="Product Image"
+				className="img-fluid"
+				height={200}
+				width={200}
+			  />
+			</div>
+		  </div>
+		</div>
+	  );
+	} else {
+	  return null; // Return null if product name is not present in data
+	}
+  })}
   
 
-		  </dl>
-        </div>
-        <div className="col-sm-4">
-          <img
-            src={record.imagepath}
-            alt="Product Image"
-            className="img-fluid"
-			height={200}
-			width={200}
-          />
-        </div>
-      </div>
-    </div>
-  ))}
 </div>
 
 
